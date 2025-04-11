a9e3e2757870f546741c0c403520459d42e73327
jonathan essis <jonathanessis94@gmail.com>
2025-04-10 21:36:56 -0400

    added missing return in points

--- receipt-processor-app.py
+++ receipt-processor-app.py
@@ -21,3 +21,18 @@ def process_receipt():
     receipts[id] = receipt
     
     return jsonify({'id': id}), 200
+
+
+@app.route('/receipts/<id>/points', methods=['GET'])
+def get_points(id):
+    if id not in receipts:
+        return jsonify({'error': 'No receipt found for that ID'}), 404
+    
+    points = calculate_points(id)
+
+    return jsonify({'points': points}), 200
+
+    
+def calculate_points(id):
+    receipt = receipts.get(id)
+    
